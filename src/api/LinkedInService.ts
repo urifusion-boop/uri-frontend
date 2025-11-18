e53414d8b456d04d34c0ee7eccfa5aa77c148975
oreakinwole <oreakinwole@gmail.com>
2025-11-18 16:16:36 +0100

    fix

--- src/api/LinkedInService.ts
+++ src/api/LinkedInService.ts
@@ -1,9 +1,9 @@
 import { UriHttpClient } from '@/configs/http.config';
-import { linkedinApiRoutes } from '@/constants/routes/linkedinRoutes';
+import { linkedinApiRoutes } from '@/constants/routes/linkedInRoutes';
 import { AxiosResponse } from 'axios';
 import { LinkedinFetchResponseDto } from '@/models/dtos/LinkedinDto';
 
-export class LinkedinService {
+export class LinkedInService {
   static async fetchPosts(keyword: string, maxPosts: number = 10): Promise<LinkedinFetchResponseDto> {
     const response: Awaited<AxiosResponse<LinkedinFetchResponseDto>> = await UriHttpClient.getClient().get(
       `${linkedinApiRoutes.fetchPosts}?keyword=${encodeURIComponent(keyword)}&max_posts=${maxPosts}`
