28818a798cfe16b23f556085caae15d3d14d8a66
TheSoftNode <thesoftnode@gmail.com>
2026-01-02 16:28:58 +0100

    fix spam urls

--- src/constants/routes/spamLeadsRoutes.ts
+++ src/constants/routes/spamLeadsRoutes.ts
@@ -6,12 +6,12 @@ const URI_BACKEND_SVC_PATH = BackendUrlEnum.INSIGHTS;
 type ISpamLeadsApi = 'getSpamLeads' | 'getSpamLeadById' | 'promoteToLead' | 'moveToSpam' | 'getStats' | 'updateNotes';
 
 const rawSpamLeadsApiRoutes: Record<ISpamLeadsApi, string> = {
-  getSpamLeads: '/spam-leads',
-  getSpamLeadById: '/spam-leads/:spam_id',
-  promoteToLead: '/spam-leads/:spam_id/promote',
-  moveToSpam: '/leads/:lead_id/move-to-spam',
-  getStats: '/spam-leads/stats',
-  updateNotes: '/spam-leads/:spam_id/notes',
+  getSpamLeads: '/lead/spam-leads',
+  getSpamLeadById: '/lead/spam-leads/:spam_id',
+  promoteToLead: '/lead/spam-leads/:spam_id/promote',
+  moveToSpam: '/lead/leads/:lead_id/move-to-spam',
+  getStats: '/lead/spam-leads/stats',
+  updateNotes: '/lead/spam-leads/:spam_id/notes',
 };
 
 export const spamLeadsApiRoutes: Record<ISpamLeadsApi, string> = RouteHelper.createRoutes(URI_BACKEND_SVC_PATH, rawSpamLeadsApiRoutes);
